6f6ab997f4176ad7c5722a8802bfa2b9420f147a
Theo <73323028+kkwondev@users.noreply.github.com>
2024-11-13 05:03:07 +0000

    [ADD] Best product

--- src/components/common/element/Product.tsx
+++ src/components/common/element/Product.tsx
@@ -1,5 +1,31 @@
 import { Product as ProductData } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Product(product: ProductData) {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/product/${product.uuid}`}>
+      <Image
+        src={product.rep_image.url}
+        alt={product.name}
+        width={600}
+        height={600}
+      />
+      <div className='flex flex-col p-2 text-sm tracking-tighter'>
+        <span className='text-[#474e5c]'>{product.name}</span>
+        <div className='flex gap-1 text-sm mb-1'>
+          <span className='text-[#ff5280] font-medium'>
+            {((product.original_price.number - product.price.number) /
+              product.original_price.number) *
+              100}
+            %
+          </span>
+          <span className='font-medium text-[#1b202a]'>
+            {product.price.string}
+          </span>
+        </div>
+        <span className='text-xs text-[#1b202a]'>{product?.color[0].name}</span>
+      </div>
+    </Link>
+  );
 }
--- src/components/main/BestProduct.tsx
+++ src/components/main/BestProduct.tsx
@@ -6,8 +6,10 @@ export default async function BestProduct() {
 
   return (
     <article className='flex flex-col gap-4'>
-      <h2 className='text-xl font-semibold'>이런 상품 좋아하실 것 같아요</h2>
-      <div className='grid grid-cols-2'>
+      <h2 className='text-xl font-semibold px-2 tracking-tighter'>
+        이런 상품 좋아하실 것 같아요
+      </h2>
+      <div className='grid grid-cols-2 gap-y-4 gap-x-[2px]'>
         {data.map((bestItem) => (
           <Product key={bestItem.uuid} {...bestItem.product} />
         ))}
