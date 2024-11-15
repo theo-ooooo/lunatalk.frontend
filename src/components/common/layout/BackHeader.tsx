83245054bec356d71bdb68fb05b089da2de22cd1
Theo <73323028+kkwondev@users.noreply.github.com>
2024-11-15 01:23:31 +0000

    [UPDATE] backHeader fixTitle 추가

--- src/components/common/layout/BackHeader.tsx
+++ src/components/common/layout/BackHeader.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
+const fixTitle = {
+  "/": "메인",
+  "/search": "검색",
+  "/cart": "장바구니",
+  "/mypage": "마이페이지",
+};
+
 export default function BackHeader() {
+  const pathname = usePathname();
   const params = useSearchParams();
   const router = useRouter();
-  const title = params.get("title") || "";
+  const title =
+    fixTitle[pathname as keyof typeof fixTitle] || params.get("title") || "";
   return (
     <div className='flex items-center justify-center h-11'>
       <div className='mr-auto flex-shrink-0 z-10' onClick={() => router.back()}>
