2610d10e765869de4d98d17ebc1f5d456f05fc6b
Theo <73323028+kkwondev@users.noreply.github.com>
2024-11-15 01:27:57 +0000

    [UPDATE] fixTitle modify

--- src/components/common/layout/BackHeader.tsx
+++ src/components/common/layout/BackHeader.tsx
@@ -3,19 +3,26 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
-const fixTitle = {
-  "/": "메인",
-  "/search": "검색",
-  "/cart": "장바구니",
-  "/mypage": "마이페이지",
-};
+// const fixTitle = {
+//   "/": "메인",
+//   "/search": "검색",
+//   "/cart": "장바구니",
+//   "/mypage": "마이페이지",
+// };
+const fixTitle = [
+  { pathname: "/search", title: "검색" },
+  { pathname: "/cart", title: "장바구니" },
+  { pathname: "/mypage", title: "마이페이지" },
+];
 
 export default function BackHeader() {
   const pathname = usePathname();
   const params = useSearchParams();
   const router = useRouter();
-  const title =
-    fixTitle[pathname as keyof typeof fixTitle] || params.get("title") || "";
+
+  const fixTitleItem = fixTitle.find((item) => item.pathname === pathname);
+  const title = fixTitleItem?.title || params.get("title") || "";
+
   return (
     <div className='flex items-center justify-center h-11'>
       <div className='mr-auto flex-shrink-0 z-10' onClick={() => router.back()}>
